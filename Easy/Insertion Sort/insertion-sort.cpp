ecec9ae066c5a1410fad912559360b988c80cb75
AakashKumar-Code <155383153+AakashKumar-Code@users.noreply.github.com>
2024-01-03 18:29:50 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Insertion Sort/insertion-sort.cpp
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+// C program for insertion sort
+#include <stdio.h>
+#include <math.h>
+
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    
+    int N;
+    void swap(int &a, int &b){
+        int temp=a;
+        a=b;
+        b=temp;
+        return;
+    }
+    void insert(int arr[], int i)
+    {
+        //code here
+        int j=i;
+        while(j>0 && arr[j-1]>arr[j]){
+            swap(arr[j], arr[j-1]);
+            j--;
+        }
+        return;
+    }
+     public:
+    //Function to sort the array using insertion sort algorithm.
+    void insertionSort(int arr[], int n)
+    {
+        //code here
+        N=n;
+        for(int i=0; i<n; i++){
+            insert(arr, i);
+        }
+        return;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int arr[1000],n,T,i;
+
+    scanf("%d",&T);
+
+    while(T--){
+
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+
+    Solution ob;
+    ob.insertionSort(arr, n);
+    printArray(arr, n);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
